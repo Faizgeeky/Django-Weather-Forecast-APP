feae515258c7c511a668f73ca55e81248eb12d11
Faizgeeky <nandoliyafaiz429@gmail.com>
2024-08-29 20:38:27 +0530

    fix : deploy setup and css

--- wforecast/urls.py
+++ wforecast/urls.py
@@ -5,5 +5,4 @@ from django.urls import path, include
 urlpatterns = [
     # path("admin/", admin.site.urls),
     path("",include('api.urls'), name="index"),
-    path('api/', include('api.urls'), name="api_endpoints"),
 ]
