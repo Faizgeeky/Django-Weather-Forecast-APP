25be3a1f9a4d9a76e83edf6a3fcceb9c1fbd2bd4
Faizgeeky <nandoliyafaiz429@gmail.com>
2024-08-29 17:57:52 +0530

    feat : data flow completed

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.check_weather, name='index'),
+    path('', views.check_weather, name='check_weather'),
+    path('forecast/', views.forecast , name='weatherforecast')
 
    
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.conf import settings
 from .models import CityWeather
 import requests
+import datetime
+from django.db.models import Avg, Max, Min
+
 
 api_key = settings.WEATHER_API
 url = 'http://api.weatherapi.com/v1'
@@ -30,24 +33,65 @@ def get_weather(cities):
         return None
 
 def check_weather(request):
-    cities = ['maxico','london']
-    weather_data = get_weather(cities)
-    print("Data us", weather_data)
-    try:
-        city_weather_instances = [
-            CityWeather(
-                city=data['city'],
-                temp_c=data['temp_c'],
-                temp_f=data['temp_f'],
-                date=data['date']
-            ) for data in weather_data
-        ]
+    if request.method == "GET":
+        return render(request,"index.html")
+    elif request.method == "POST":
+        # get cities need forecast for
+        cities = request.POST.get('cities').split(",")
         
-        CityWeather.objects.bulk_create(city_weather_instances)
-    except Exception as e:
-        return render(request, "index.html", {
-            "error": f"Something went wrong adding data: {str(e)}",
-            "data": weather_data
-        })
 
-    return render(request, 'index.html', {"data": weather_data})
+        if cities:
+            weather_data = get_weather(cities)
+            print("Data us", weather_data)
+            try:
+                city_weather_instances = [
+                    CityWeather(
+                        city=data['city'],
+                        temp_c=data['temp_c'],
+                        temp_f=data['temp_f'],
+                        date=data['date']
+                    ) for data in weather_data
+                ]
+                
+                CityWeather.objects.bulk_create(city_weather_instances)
+            except Exception as e:
+                return render(request, "index.html", {
+                    "error": f"Something went wrong adding data: {str(e)}",
+                    "data": weather_data
+                })
+        else:
+            return render(request,'index.html',{"message":"Cities not found in Req body!"} )
+
+        return render(request, 'index.html', {"data": weather_data})
+    
+
+
+def forecast(request):
+    if request.method == "GET":
+        return render(request, 'forecast.html')
+    elif request.method == "POST":
+        cities = request.POST.get('cities').split(",")
+
+
+        last_24_hrs =  datetime.datetime.now() - datetime.timedelta(days=1)
+
+        print(last_24_hrs)
+        if cities:
+            city_object = {}
+            for city in cities:
+                data = CityWeather.objects.filter(city = city ,date__gt=last_24_hrs)
+                data = data.aggregate(
+                    avg_temp_c = Avg('temp_c'),
+                    max_temp_c = Avg('temp_c'),
+                    min_temp_c = Avg('temp_c'),
+
+                    avg_temp_f = Avg('temp_f'),
+                    max_temp_f = Avg('temp_f'),
+                    min_temp_f = Avg('temp_f'),
+
+                )
+                
+                city_object[city] = data
+
+    print(city_object)        
+    return render(request, 'forecast.html', {'data':city_object})
